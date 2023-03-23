16c4df26eab641865e7dccfacc9b5571f0b88981
cooltu <cooltu@codingtu.com>
2023-03-23 12:56:40 +0800

    coreactivity创建

--- cooltu/cooltu_lib4a/src/main/java/cooltu/lib4a/activity/CoreActivity.java
+++ cooltu/cooltu_lib4a/src/main/java/cooltu/lib4a/activity/CoreActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import cooltu.lib4a.tools.ActTool;
 import cooltu.lib4a.tools.ScreenAdaptationTool;
 import cooltu.lib4a.tools.StatusBarTool;
 
@@ -15,9 +16,50 @@ public class CoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ScreenAdaptationTool.setCustomDensity(this);
         initStatusBar();
+        ActivityManager.getInstance().add(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityManager.getInstance().remove(this);
     }
 
     protected void initStatusBar() {
         StatusBarTool.translucentAndDark(this);
     }
+
+    /**************************************************
+     *
+     * 对finish方法做扩展
+     *
+     **************************************************/
+    @Override
+    public void finish() {
+        beforeFinish();
+        super.finish();
+        setFinishAnimation();
+        afterFinish();
+    }
+
+    protected void finishToNewPage() {
+        beforeFinish();
+        super.finish();
+        afterFinish();
+    }
+
+    //finish之后调用
+    protected void afterFinish() {
+    }
+
+    //设置finish动画
+    protected void setFinishAnimation() {
+        ActTool.actRightOut(this);
+    }
+
+    //finish之前调用
+    protected void beforeFinish() {
+    }
+
 }
