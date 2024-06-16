0bf32022bd4c940ac4f17d0deacb61d3c4b239ba
WIN-20240504QQN\Administrator <82654060@qq.com>
2024-06-16 12:13:56 +0800

    v2.0.172 删除多余的代码

--- cooltu/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/form/FormHandleCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.codingtu.cooltu.lib4a.form;
-
-import android.os.Message;
-
-import java.util.Map;
-
-public interface FormHandleCallBack {
-    void handleMessage(Message msg, Map<String, Object[]> linkMap);
-}
