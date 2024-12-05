7864533eb1e11121c8e3110897bf759844afb87d
codingtu <cooltu@codingtu.com>
2024-12-05 16:36:30 +0800

    v2.0.228 修改base64转string的问题

--- cooltu/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/cryption/Base64Tool.java
+++ cooltu/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/cryption/Base64Tool.java
@@ -5,15 +5,15 @@ import android.util.Base64;
 public class Base64Tool {
 
     public static byte[] encodeToBase64(byte[] bytes) {
-        return android.util.Base64.encode(bytes, android.util.Base64.DEFAULT);
+        return Base64.encode(bytes, android.util.Base64.DEFAULT);
     }
 
     public static String encodeToBase64Str(byte[] bytes) {
-        return android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT);
+        return new String(encodeToBase64(bytes));
     }
 
     public static byte[] decodeToBase64(byte[] bytes) {
-        return android.util.Base64.decode(bytes, Base64.DEFAULT);
+        return Base64.decode(bytes, Base64.DEFAULT);
     }
 
     public static String decodeToBase64Str(byte[] bytes) {
